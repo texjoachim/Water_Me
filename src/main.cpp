228dcd1a1fc752481443525b1a34392ea467273b
Joachim Ziebs <joachim@ziebs.net>
2017-12-12 20:43:49 +0100

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// Water Me
+// a small device to tell you when to water your plant
+
+#include <Arduino.h>
+#include <flyingFishHelper.h>
+#include <rgbLED.h>
+
+// our objects
+
+rgbLED LED(D2, D3, D4);
+flyingFish MoistureSensor(D5, A1);
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
